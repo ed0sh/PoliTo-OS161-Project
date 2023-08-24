4fc4bdfabb219a4db478d20e284b7af8b51d583f
Jelly1600 <118910344+Jelly1600@users.noreply.github.com>
2023-08-24 11:56:21 +0200

    feat(vm_tlb): tlb management completed

--- src/kern/include/vm_tlb.h
+++ src/kern/include/vm_tlb.h
@@ -1,10 +1,8 @@
-/* TLB MANAGEMENT */
+#ifndef _VM_TLB_H_
+#define _VM_TLB_H_
 
-// Round Robin TLB replacement algorithm
 int tlb_get_rr_victim(void);
-
-uint32_t tlb_search(uint32_t entryhi);
-void tlb_load(pt_entry *pt_entry, uint32_t entryhi);
+void tlb_load(uint32_t entryhi, uint32_t entrylo, int perm);
 void tlb_invalidate(void);
-int vm_fault(int faulttype, vaddr_t faultaddress);
-static void my_vm_can_sleep(void);
+
+#endif 
--- src/kern/vm/vm_tlb.c
+++ src/kern/vm/vm_tlb.c
@@ -1,21 +1,15 @@
-/*  TLB MANAGEMENT
+/*  TLB MANAGEMENT  */
 
-    Use of: tlb_random, tlb_write, tlb_read, tlb_probe
-    
-    Virtual Page in ENTRYHI, Physical Page in ENTRYLO
-
-    "spl" stands for "set priority level"
-    aggiungi da dumbvm.c  splhigh e splx rige 320-180 tipo --> servono ogni volta che fai tlb_write
-*/
 
 #include <types.h>
 #include <elf.h>
 #include <tlb.h>
 #include <vm_tlb.h>
+#include <spl.h>
 
-// round robin algorithm for TLB replacement
-int tlb_get_rr_victim(void)
-{
+
+// Round Robin TLB replacement algorithm
+int tlb_get_rr_victim(void) {
     int victim;
     static unsigned int next_victim = 0;
     victim = next_victim;
@@ -23,47 +17,14 @@ int tlb_get_rr_victim(void)
     return victim;
 }
 
-// Search in TLB - return physical page 
-uint32_t tlb_search(uint32_t entryhi)
-{
-    int index;
-    uint32_t v_hi, p_lo;
-    index = tlb_probe(entryhi, 0);
-    if (index < 0)
-    { // TLB MISS
-        // call TLB miss exception (EX_TLBL o EX_TLBS) - (you must go in pt and then eventually in memory)
-        vm_fault(EX_TLBL, v_hi); 
-    }
-    else
-    { // TLB HIT
-        tlb_read(&v_hi, &p_lo, index);
-        if (!(p_lo & TLBLO_VALID))  // invalid
-        { 
-            // call TLB miss exception (EX_TLBL o EX_TLBS) - (you must go in pt and then eventually in memory)
-            vm_fault(EX_TLBL, v_hi); 
-        }
-
-        #if OPT_DEBUG_PAGING
-            if (v_hi != entryhi)
-            {
-                panic("TLB search not working right.\n");
-            }
-        #endif
-
-        return p_lo;
-    }
-}
 
 // Load a new entry in tlb (if there is space use it, otherwise free it with RR algorithm)
-void tlb_load(pt_entry *pt_entry, uint32_t entryhi){ //vedi meglio per cosa passare come argomenti
-    uint32_t v_hi, p_lo, perm, entrylo;
+void tlb_load(uint32_t entryhi, uint32_t entrylo, int perm) {
+    uint32_t v_hi, p_lo;
     int i, victim=-1, spl;
 
     // Disable interrupts on this CPU while frobbing the TLB
 	spl = splhigh();
-
-    entrylo = (uint32_t) pt_entry->paddr;
-    perm = pt_entry->perm;
 
     for (i = 0; i < NUM_TLB, victim = -1; i++) {
         tlb_read(&v_hi, &p_lo, i);
@@ -86,6 +47,7 @@ void tlb_load(pt_entry *pt_entry, uint32_t entryhi){ //vedi meglio per cosa pass
 
     return;
 }
+
 
 void tlb_invalidate(void) {
     int i, spl;
@@ -118,23 +80,3 @@ void tlb_invalidate(void) {
     return;
 }
 
-// La funzione vm_fault() era definita in dumbvm.c --> dobbiamo rifarla!! 
-// La vm_fault() viene usata nel file trap.c in mips_trap().
-// Gestione degli errori:
-// EX_MOD    1  TLB Modify (write to read-only page) 
-// EX_TLBL   2  TLB miss on load 
-// EX_TLBS   3  TLB miss on store 
-int vm_fault(int faulttype, vaddr_t faultaddress){
-
-}
-
-// Check if we're in a context that can sleep - Assert that sleeping is ok
-static void my_vm_can_sleep(void) {
-    if (CURCPU_EXISTS()) {
-		/* must not hold spinlocks */
-		KASSERT(curcpu->c_spinlocks == 0);
-
-		/* must not be in an interrupt handler */
-		KASSERT(curthread->t_in_interrupt == 0);
-	}
-}
